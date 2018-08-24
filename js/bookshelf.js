841da960bd09f6d42972efcfbafe72ae0a897fa0
Aki Sipovaara <mushut@gmail.com>
2018-08-24 16:56:57 +0300

    Updated style.css and added forms for removing and reordering book table.

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-    $("#form").submit(function(event){
+    // Add element to the table
+    $("#form_add").submit(function(event){
         let book_name = $("#book_name").val();
         let book_authors = $("#book_authors").val();
 
         let markup = "<tr><td>" + book_name + "</td><td>" + book_authors + "</td></tr>";
 
         $("#bookshelf_tbody").append(markup);
-        event.preventDefault();
+        event.preventDefault(); // Prevents page to refresh (and delete the added element)
     });
+
+    // Remove element from the table
+    // TBD
+
+    // Reorder elements of the table
+    // TBD
 });
