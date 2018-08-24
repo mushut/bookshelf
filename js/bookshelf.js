2fe3b6b15899bf3fa277e51952e295665828fa9d
Aki Sipovaara <mushut@gmail.com>
2018-08-24 16:12:15 +0300

    Adding elements to table works now (submit doesn't refresh the page)

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-    $("#form").submit(function(){
+    $("#form").submit(function(event){
         let book_name = $("#book_name").val();
         let book_authors = $("#book_authors").val();
 
-        $("#bookshelf_tbody").append("<tr><td>",book_name,"</td><td>",book_authors,"</td></tr>");
+        let markup = "<tr><td>" + book_name + "</td><td>" + book_authors + "</td></tr>";
+
+        $("#bookshelf_tbody").append(markup);
+        event.preventDefault();
     });
 });
